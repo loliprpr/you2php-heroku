b6dbe2e8ffad5ab48e98b17f7c1983e95092a252
loliprpr <41909988+loliprpr@users.noreply.github.com>
2018-07-31 08:38:01 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', getenv("AIzaSyANWu8ZKGjDYoBoIIhzU62RV-zids2NrSQ"));
-define('GJ_CODE', getenv("JPN"));
+define('GJ_CODE', getenv("JP"));
 define('SITE_NAME',getenv("Youneed"));
 define('TITLENAME', getenv("Youneed"));
 define('EN2DEKEY',getenv("z792708571"));
